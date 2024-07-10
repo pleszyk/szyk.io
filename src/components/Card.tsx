9e66e408f641671c87d155b3df7b397f2c8e52ca
pleszyk <pleszyk@gmail.com>
2024-07-09 21:23:37 -0400

    first commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,34 +1,39 @@
-import { HiOutlineExternalLink } from 'react-icons/hi';
+import { HiOutlineExternalLink } from 'react-icons/hi'
 import ReactGA from 'react-ga4'
-ReactGA.initialize("G-M9Q2K7J9MB");
-import {cardType} from "../types.ts";
+
+ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID)
+import { cardType } from '../types.ts'
 
 function Card(props: cardType) {
+    function useAnalyticsEventTracker() {
+        ReactGA.send({
+            hitType: 'pageview',
+            page: props.link,
+            title: props.title,
+        })
+    }
 
-  function useAnalyticsEventTracker(){
-    ReactGA.send({ hitType: "pageview", page: props.link, title: props.title });
-  }
+    return (
+        <a
+            onClick={useAnalyticsEventTracker}
+            href={props.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative block bg-blend-overlay bg-black bg-opacity-25 w-full h-64 overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${props.photo})` }}
+        >
+            <span className="absolute right-4 top-4 z-10 items-center rounded-lg bg-[#e7ecef] p-1.5 text-s text-black">
+                <HiOutlineExternalLink />
+            </span>
 
-  return (
-    <a
-      onClick={useAnalyticsEventTracker}
-      href={props.link}
-      target='_blank'
-      rel='noopener noreferrer'
-      className='relative block bg-blend-overlay bg-black bg-opacity-25 w-full h-64 overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat'
-      style={{ backgroundImage: `url(${props.photo})` }}
-    >
-      <span className='absolute right-4 top-4 z-10 items-center rounded-lg bg-[#e7ecef] p-1.5 text-s text-black'>
-        <HiOutlineExternalLink />
-      </span>
+            <div className="absolute italic font-light text-gray-200 bottom-4 left-3 pr-20">
+                <h3 className="text-2xl font-medium">{props.title}</h3>
 
-      <div className='absolute italic font-light text-gray-200 bottom-4 left-3 pr-20'>
-        <h3 className='text-2xl font-medium'>{props.title}</h3>
-
-        <p className='text-sm'>{props.desc}</p>
-        {props.spin && <p className='text-xs'>(30s spin-up delay)</p>}
-      </div>
-    </a>
-  );
+                <p className="text-sm">{props.desc}</p>
+                {props.spin && <p className="text-xs">(30s spin-up delay)</p>}
+            </div>
+        </a>
+    )
 }
-export default Card;
+
+export default Card
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,26 @@
-import Logo from '../assets/sailboat.svg';
+import Logo from '../assets/sailboat.svg'
 
 function Nav() {
-  return (
-    <nav
-      aria-label='Site Nav'
-      className='mx-auto flex max-w-7xl items-center justify-between p-4'
-    >
-      <div
-        className='inline-flex h-12 items-center justify-center'
-      >
-        <span className='sr-only'>Logo</span>
-        <img className='w-14' src={Logo} alt="logo" />
-      </div>
+    return (
+        <nav
+            aria-label="Site Nav"
+            className="mx-auto flex max-w-7xl items-center justify-between p-4"
+        >
+            <div className="inline-flex h-12 items-center justify-center">
+                <span className="sr-only">Logo</span>
+                <img className="w-14" src={Logo} alt="logo" />
+            </div>
 
-      <ul className='flex items-center gap-2 text-sm font-medium text-gray-500'>
-        <li>
-          <div className='rounded-lg cursor-pointer px-3 py-0.5'>
-            Home
-          </div>
-          <div className="h-[2px] w-9 m-auto bg-gray-300 rounded-md"></div>
-        </li>
-      </ul>
-    </nav>
-  );
+            <ul className="flex items-center gap-2 text-sm font-medium text-gray-500">
+                <li>
+                    <div className="rounded-lg cursor-pointer px-3 py-0.5">
+                        Home
+                    </div>
+                    <div className="h-[2px] w-9 m-auto bg-gray-300 rounded-md"></div>
+                </li>
+            </ul>
+        </nav>
+    )
 }
-export default Nav;
+
+export default Nav
