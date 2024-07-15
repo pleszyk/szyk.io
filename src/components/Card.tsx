8f7e1405fa76e8d16767928734f23db5c3e9a543
pleszyk <pleszyk@gmail.com>
2024-07-14 21:20:35 -0400

    first commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,35 +5,35 @@ ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID)
 import { cardType } from '../types.ts'
 
 function Card(props: cardType) {
-    function useAnalyticsEventTracker() {
-        ReactGA.send({
-            hitType: 'pageview',
-            page: props.link,
-            title: props.title,
-        })
-    }
+  function useAnalyticsEventTracker() {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: props.link,
+      title: props.title,
+    })
+  }
 
-    return (
-        <a
-            onClick={useAnalyticsEventTracker}
-            href={props.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="relative block bg-blend-overlay bg-black bg-opacity-25 w-full h-64 overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${props.photo})` }}
-        >
+  return (
+    <a
+      onClick={useAnalyticsEventTracker}
+      href={props.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="relative block bg-blend-overlay bg-black bg-opacity-25 w-full h-64 overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${props.photo})` }}
+    >
             <span className="absolute right-4 top-4 z-10 items-center rounded-lg bg-[#e7ecef] p-1.5 text-s text-black">
                 <HiOutlineExternalLink />
             </span>
 
-            <div className="absolute italic font-light text-gray-200 bottom-4 left-3 pr-20">
-                <h3 className="text-2xl font-medium">{props.title}</h3>
+      <div className="absolute italic font-light text-gray-200 bottom-4 left-3 pr-20">
+        <h3 className="text-2xl font-medium">{props.title}</h3>
 
-                <p className="text-sm">{props.desc}</p>
-                {props.spin && <p className="text-xs">(30s spin-up delay)</p>}
-            </div>
-        </a>
-    )
+        <p className="text-sm">{props.desc}</p>
+        {props.spin && <p className="text-xs">(30s spin-up delay)</p>}
+      </div>
+    </a>
+  )
 }
 
 export default Card
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,84 +2,84 @@ import { AiFillLinkedin } from 'react-icons/ai'
 import { DiGithubFull } from 'react-icons/di'
 import { FaAws } from 'react-icons/fa'
 import {
-    SiMongodb,
-    SiExpress,
-    SiReact,
-    SiNodedotjs,
-    SiJavascript,
-    SiTypescript,
-    SiHtml5,
-    SiCss3,
-    SiNextdotjs,
-    SiTailwindcss,
+  SiMongodb,
+  SiExpress,
+  SiReact,
+  SiNodedotjs,
+  SiJavascript,
+  SiTypescript,
+  SiHtml5,
+  SiCss3,
+  SiNextdotjs,
+  SiTailwindcss,
 } from 'react-icons/si'
 import ReactGA from 'react-ga4'
 
 ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID)
 
 function Header() {
-    function githubClick() {
-        ReactGA.send({
-            hitType: 'pageview',
-            page: 'https://github.com/pleszyk',
-            title: 'Github',
-        })
-    }
+  function githubClick() {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: 'https://github.com/pleszyk',
+      title: 'Github',
+    })
+  }
 
-    function linkedinClick() {
-        ReactGA.send({
-            hitType: 'pageview',
-            page: 'https://www.linkedin.com/in/szyk/',
-            title: 'LinkedIn',
-        })
-    }
+  function linkedinClick() {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: 'https://www.linkedin.com/in/szyk/',
+      title: 'LinkedIn',
+    })
+  }
 
-    return (
-        <div className="w-full col-span-2 p-1">
-            <div
-                className="relative italic block bg-blend-overlay bg-black bg-opacity-25 overflow-hidden h-64 rounded-xl bg-[url('assets/header.svg')] bg-cover bg-center bg-no-repeat">
-                <div className="absolute p-8 text-gray-100">
-                    <h3 className="text-5xl rounded-lg font-medium">Paul Leszyk</h3>
-                    <p className="text-lg font-normal ">B.Sc Computer Science</p>
-                    <p className="text-xs text-gray-100 mt-1 font-normal">
-                        Javascript, Typescript, HTML, CSS, React, <br /> Next.js, TailwindCSS, MongoDB, Node.js
-                        & AWS
-                    </p>
-                    <div className="flex text-[#062a4e] text-xl p-1 pt-2">
-                        <SiJavascript />
-                        <SiTypescript className="ml-2" />
-                        <SiNextdotjs className="ml-2" />
-                        <SiReact className="ml-2" />
-                        <SiNodedotjs className="ml-2" />
-                        <SiExpress className="ml-2" />
-                        <FaAws className="ml-2" />
-                        <SiMongodb className="ml-2" />
-                        <SiHtml5 className="ml-2" />
-                        <SiCss3 className="ml-2" />
-                        <SiTailwindcss className="ml-2" />
-                    </div>
-                </div>
-                <a
-                    onClick={githubClick}
-                    href="https://github.com/pleszyk"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="absolute transition xl:hover:scale-[1.05] left-4 bottom-4 items-center rounded-lg bg-[#e7ecef] p-1.5 text-2xl text-black"
-                >
-                    <DiGithubFull />
-                </a>
-                <a
-                    onClick={linkedinClick}
-                    href="https://www.linkedin.com/in/szyk/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="absolute transition xl:hover:scale-[1.05] left-16 bottom-4 items-center rounded-lg bg-[#e7ecef] p-1.5 text-2xl text-black"
-                >
-                    <AiFillLinkedin />
-                </a>
-            </div>
+  return (
+    <div className="w-full col-span-2 p-1">
+      <div
+        className="relative italic block bg-blend-overlay bg-black bg-opacity-25 overflow-hidden h-64 rounded-xl bg-[url('assets/header.svg')] bg-cover bg-center bg-no-repeat">
+        <div className="absolute p-8 text-gray-100">
+          <h3 className="text-5xl rounded-lg font-medium">Paul Leszyk</h3>
+          <p className="text-lg font-normal ">B.Sc Computer Science</p>
+          <p className="text-xs text-gray-100 mt-1 font-normal">
+            Javascript, Typescript, HTML, CSS, React, <br /> Next.js, TailwindCSS, MongoDB, Node.js
+            & AWS
+          </p>
+          <div className="flex text-[#062a4e] text-xl p-1 pt-2">
+            <SiJavascript />
+            <SiTypescript className="ml-2" />
+            <SiNextdotjs className="ml-2" />
+            <SiReact className="ml-2" />
+            <SiNodedotjs className="ml-2" />
+            <SiExpress className="ml-2" />
+            <FaAws className="ml-2" />
+            <SiMongodb className="ml-2" />
+            <SiHtml5 className="ml-2" />
+            <SiCss3 className="ml-2" />
+            <SiTailwindcss className="ml-2" />
+          </div>
         </div>
-    )
+        <a
+          onClick={githubClick}
+          href="https://github.com/pleszyk"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute transition xl:hover:scale-[1.05] left-4 bottom-4 items-center rounded-lg bg-[#e7ecef] p-1.5 text-2xl text-black"
+        >
+          <DiGithubFull />
+        </a>
+        <a
+          onClick={linkedinClick}
+          href="https://www.linkedin.com/in/szyk/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute transition xl:hover:scale-[1.05] left-16 bottom-4 items-center rounded-lg bg-[#e7ecef] p-1.5 text-2xl text-black"
+        >
+          <AiFillLinkedin />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Header
